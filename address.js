2ad9d916e546571f7244ad887e2d0a4f443be5f9
VershaR1791 <84694664+VershaR1791@users.noreply.github.com>
2021-11-27 22:52:13 -0500

    Add files via upload

---
+++ address.js
@@ -0,0 +1,125 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "index": 173510,
+        "Gross_Square_Feet": 71500,
+        "Sale_Price": 5750000,
+        "Address": "119-121 WEST 28 STREET                   ",
+        "Building_Class_Category": "23  LOFT BUILDINGS                          ",
+        "Borough": 1,
+        "Neighborhood": "FASHION                  ",
+        "Zip_Code": "10001",
+        "Class_Bins": "APARTMENT",
+        "latlong": "40.7466515, -73.99141639999999",
+        "Latitude": "40.7466515",
+        "Longitude": "-73.99141639999999"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          "-73.99141639999999",
+          "40.7466515"
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "index": 167169,
+        "Gross_Square_Feet": 5808,
+        "Sale_Price": 3854000,
+        "Address": "315 WEST 29TH STREET                     ",
+        "Building_Class_Category": "07  RENTALS - WALKUP APARTMENTS             ",
+        "Borough": 1,
+        "Neighborhood": "CHELSEA                  ",
+        "Zip_Code": "10001",
+        "Class_Bins": "RENTAL APARTMENT",
+        "latlong": "40.7496204, -73.9965162",
+        "Latitude": "40.7496204",
+        "Longitude": "-73.9965162"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          "-73.9965162",
+          "40.7496204"
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "index": 178675,
+        "Gross_Square_Feet": 19053,
+        "Sale_Price": 12300000,
+        "Address": "249 BROOME STREET                        ",
+        "Building_Class_Category": "07  RENTALS - WALKUP APARTMENTS             ",
+        "Borough": 1,
+        "Neighborhood": "LOWER EAST SIDE          ",
+        "Zip_Code": "10002",
+        "Class_Bins": "RENTAL APARTMENT",
+        "latlong": "40.7178541, -73.98996269999999",
+        "Latitude": "40.7178541",
+        "Longitude": "-73.98996269999999"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          "-73.98996269999999",
+          "40.7178541"
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "index": 176393,
+        "Gross_Square_Feet": 5696,
+        "Sale_Price": 999900,
+        "Address": "187 EAST BROADWAY                        ",
+        "Building_Class_Category": "14  RENTALS - 4-10 UNIT                     ",
+        "Borough": 1,
+        "Neighborhood": "LOWER EAST SIDE          ",
+        "Zip_Code": "10002",
+        "Class_Bins": "RENTAL APARTMENT",
+        "latlong": "40.7139909, -73.988908",
+        "Latitude": "40.7139909",
+        "Longitude": "-73.988908"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          "-73.988908",
+          "40.7139909"
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "index": 171493,
+        "Gross_Square_Feet": 5390,
+        "Sale_Price": 3288096,
+        "Address": "129 1 AVENUE                             ",
+        "Building_Class_Category": "07  RENTALS - WALKUP APARTMENTS             ",
+        "Borough": 1,
+        "Neighborhood": "EAST VILLAGE             ",
+        "Zip_Code": "10003",
+        "Class_Bins": "RENTAL APARTMENT",
+        "latlong": "40.727483, -73.9856935",
+        "Latitude": "40.727483",
+        "Longitude": "-73.9856935"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          "-73.9856935",
+          "40.727483"
+        ]
+      }
+    }
+  ]
+}
