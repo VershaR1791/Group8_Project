2048e0310f06c0f9c0e07ab37196608daef3cf1b
VershaR1791 <84694664+VershaR1791@users.noreply.github.com>
2021-11-28 13:22:06 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,8 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
-
 // Create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,42 +8,48 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     accessToken: API_KEY
 });
 
-// Create the dark view tile layer that will be an option for our map.
+// Create the satellite view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
-let baseMaps = {
-    "Streets": streets,
-    "Satellite": satelliteStreets
-};
+// Create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid',{
     center: [40.7306, -73.9352],
-    zoom: 12,
+    zoom: 11,
     layers: [streets]
 });
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets,
+    "Dark": dark
+};
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-//  Add a marker to the map for New York City, NY.
-L.circleMarker([40.7306, -73.9352], {
-    radius: 300,
-    color: "blue",
-    fillColor: '#ffffa1'
- },).addTo(map);
+// Accessing the NY City from GeoJSON URL.
+let NYCdata = "https://raw.githubusercontent.com/VershaR1791/Group8_Project/Versha/address.json"
 
-// Accessing the NY City SalesPrice GeoJSON URL.
-let NYCData = "https://raw.githubusercontent.com/VershaR1791/Group8_Project/Versha/<XYZ>.json";
-
-// Grabbing our GeoJSON data.
-d3.json(NYCData).then(function(data) {
+d3.json(NYCdata).then(function(data){
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+  // We create a popup for each circleMarker to display the Sale Price and Address of the property
+     //  after the marker has been created and styled.
+     onEachFeature: function(feature, layer) {
+      layer.bindPopup("Address: " + feature.properties.Address + "<br>Neighborhood: " + feature.properties.Neighborhood + "<br>Zip Code : " + feature.properties.Zip_Code + "<br>Property Type : " + feature.properties.Class_Bins + "<br>Avg. Sale Price: " + feature.properties.Sale_Price);
+    }
+  }).addTo(map);
 });
